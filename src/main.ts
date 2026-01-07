3fa7915de0be17ec4050de3f5eb71252f93710ec
Welton Franco <weltonsantosfr@gmail.com>
2026-01-06 22:33:58 -0300

    configure cors to accept localhost requests

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS for requests from http://localhost:5173
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   // Enable global validation with strict options
   app.useGlobalPipes(
     new ValidationPipe({
